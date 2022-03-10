0e54f695dce56053a39d05838598b48bb09bd7db
Piero Maddaleni <pieromaddaleni03@gmail.com>
2022-03-10 10:53:56 +0000

    yes

--- out/_next/static/chunks/pages/trains/saved-94cfac9cfd59831a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[807],{1093:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/trains/saved",function(){return t(4656)}])},4656:function(n,u,t){"use strict";function r(){return"sus"}t.r(u),t.d(u,{default:function(){return r}})}},function(n){n.O(0,[774,888,179],(function(){return u=1093,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
