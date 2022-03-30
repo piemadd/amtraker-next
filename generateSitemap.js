0f6d88b26e68fddeab6177aff81cc0e9422a33a4
Piero Maddaleni <pieromaddaleni03@gmail.com>
2022-03-30 04:36:45 +0000

    fixed sitemap

--- generateSitemap.js
+++ generateSitemap.js
@@ -2,8 +2,10 @@ const { data } = require("./public/data/trainNums");
 const fs = require("fs");
 
 const dataKeys = Object.keys(data);
-const rightNow = new Intl.DateTimeFormat('en-US').format(new Date()).replaceAll('/', '-');
+//const rightNow = new Intl.DateTimeFormat('en-US').format(new Date()).replaceAll('/', '-');
 
+const now = new Date();
+const rightNow = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}`
 /*
 <url><loc>https://amtraker.com/</loc><lastmod>${rightNow}</lastmod></url><url><loc>https://amtraker.com/about</loc><lastmod>${rightNow}</lastmod></url><url><loc>https://amtraker.com/privacy.html</loc><lastmod>${rightNow}</lastmod></url><url><loc>https://amtraker.com/trains/new</loc><lastmod>${rightNow}</lastmod></url>
 */
